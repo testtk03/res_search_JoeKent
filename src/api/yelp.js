a64920b5b59e3b835004fb47ece37c254f8ca079
HuyVo <huy.vminh71@gmail.com>
2021-03-11 18:58:26 +0200

    Initial commit

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         Authorization: 
-            ''
+            'Bearer MyD9dsONXc0WA3BVK3v9EkDgWoCwNTwRJ_pQh20YdC_ntTovZuTU2DAiGFOhdUYpg1I8HkTzsAmOmCj6StqGj8xmoo7_LKpDccA2x-griQEVBIOC27D9R00Wi7tIYHYx'
     }
 });
